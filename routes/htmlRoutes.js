3b2d4458d98e49e007dbed2275361d09421179ea
Beth Ehleringer <elizabethehleringer@gmail.com>
2018-12-08 11:52:38 -0500

    simplified htmlRoutes.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,23 +2,22 @@ var db = require("../models");
 
 module.exports = function(app) {
   // Load index page
-  app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+  app.get("/", function(req, res) { 
+      res.render("index");
     });
+  
+
+  // Load index page
+  app.get("/index", function(req, res) { 
+    res.render("index");
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
-    });
+  // Load survey page
+  app.get("/survey", function(req, res) { 
+    res.render("survey");
   });
+
+
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
