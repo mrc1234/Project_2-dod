020e7d2992fcc37d463c8cd11932a9d5af4a4a5d
Max Rosen <maxcrosen@gmail.com>
2019-01-05 11:24:10 -0500

    these are the final changes

--- public/js/survey.js
+++ public/js/survey.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
   //ids association from the sign up page
   var companyName = $("#company-name");
   var position = $("#position");
@@ -10,13 +10,16 @@ $(document).ready(function() {
   var website = $("#website");
   var webPlatform = $("textarea#platform");
 
-  $("#survey-submit").on("click", function(event) {
+  $("#survey-submit").on("click", function (event) {
     event.preventDefault();
     //capture the selected yes or no answers
     var ownServer = $("input[name=own_server]:checked").val();
     var onsite = $("input[name=server_onsite]:checked").val();
     var cloudServer = $("input[name=cloud_server]:checked").val();
     var dataCollection = $("input[name=collect_user_data]:checked").val();
+    // get upload file name -- 'prep' obtains files property
+    var uploadFileName = $("#upload-file").prop("files")[0];
+    var keyName = uploadFileName.name;
     //declare a variable to save all the stored inputs
     var surveyData = {
       company_name: companyName.val().trim(),
@@ -27,11 +30,13 @@ $(document).ready(function() {
       office_number: officeNumber.val().trim(),
       social_media: socialMedia.val().trim(),
       website: website.val().trim(),
-      plate_form: webPlatform.val().trim(),
+      platform: webPlatform.val().trim(),
       own_server: ownServer,
       server_onsite: onsite,
       cloud_server: cloudServer,
-      collect_user_data: dataCollection
+      collect_user_data: dataCollection,
+      uploadFileName: uploadFileName,
+      keyName: keyName
     };
     console.log(surveyData);
     $.ajax({
@@ -41,12 +46,20 @@ $(document).ready(function() {
       type: "POST",
       url: "api/newinfo",
       data: JSON.stringify(surveyData)
-    }).done(function(results) {
-      window.location.replace('result')
-    })
-   
-      });
+    }).done(function (data) {
+      window.location.replace("result");
+      console.log(data);
+    });
 
-      
-      
+    // upload file to AWS (the code is in apiRoutes.js)    
+    var queryURL = "/api/upload";
+    $.ajax({
+      url: queryURL,
+      method: "POST"
+    }).then(function (response) {
+      console.log(response);
+    });
+  });
 });
+
+//module.exports.keyName = keyName;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,53 +1,98 @@
 var db = require("../models");
 var passport = require("../config/passport");
+//AWS
+var AWS = require('aws-sdk');
+var uuid = require('uuid');
 
-module.exports = function(app) {
+// var keyName = require("../public/js/survey");  
+//--this line above causes the following error;
+// Users/jaipy724/code/ucf/projects/Project_2-dod/public/js/survey.js:1
+// (function (exports, require, module, __filename, __dirname) { $(document).ready(function () {
+//                                                               ^
+// ReferenceError: $ is not defined
+
+module.exports = function (app) {
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
     // So we're sending the user back the route to the members page because the redirect will happen on the front end
     // They won't get this or even be able to access this page if they aren't authed
     res.json("/members");
   });
-
-
   // Get all examples
   // Get all users
-  app.get("/api/users", function(req, res) {
-    db.user.findAll({}).then(function(dbUser) {
+  app.get("/api/users", function (req, res) {
+    db.user.findAll({}).then(function (dbUser) {
       res.json(dbUser);
     });
   });
 
   // Create a new user
-  app.post("/api/newuser", function(req, res) {
+  app.post("/api/newuser", function (req, res) {
     console.log(req.body);
-    db.user.create(req.body).then(function(dbuser) {
+    db.user.create(req.body).then(function (dbuser) {
       res.json(dbuser);
     });
   });
 
   // create the new post for the survey questions
-  app.post("/api/newinfo", function(req, res, next) {
+  app.post("/api/newinfo", function (req, res, next) {
     console.log(req.body);
-    db.information.create(req.body).then(function(dbinfo) {
-     // res.json(dbinfo);
-     next()
-    //  res.redirect(303, "/result");
-     }).catch(function(err) {
-      console.log(err);
-      res.json(err);
-    });
-  }, function(req,res){
-    res.send({redirect: '/result'
+    db.information
+      .create(req.body)
+      .then(function (dbinfo) {
+        res.json(dbinfo);
+        next();
+        //res.redirect(303, "/result");
+      })
+      .catch(function (err) {
+        console.log(err);
+        res.json(err);
+      });
   });
 
+  // -----------------------------------------------
+  // Upload file selected in Survey page to AWS(S3)
+  // -----------------------------------------------
+  // Reference:
+  // Amazon Getting Started in Node.js
+  // https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/getting-started-nodejs.html
+  // Note: This code doesn't use Policy and Signature, creates a new Amazon S3 bucket and uploads an object to the bucket
+  //       Once object is uploaded then we can see the created bucket name and a message in console log like below:
+  //       >> bucketName: project2-3d257603-df4d-42c3-8395-040192083c8e
+  //       >> Successfully uploaded data to project2-3d257603-df4d-42c3-8395-040192083c8e/hello_world.txt
+  //
 
+  app.post("/api/upload", function (req, res) {
+    console.log(req.body);
+    // Create unique bucket name
+    var bucketName = 'project2-' + uuid.v4();
 
+    // Upload file name (object key)
+    var keyName = 'hello_world.txt';
 
-});
+    console.log("bucketName: " + bucketName);
+
+    // Create a promise on S3 service object
+    var bucketPromise = new AWS.S3({ apiVersion: '2006-03-01' }).createBucket({ Bucket: bucketName }).promise();
+
+    // Handle promise fulfilled/rejected states
+    bucketPromise.then(
+      function (data) {
+        // Create params for putObject call
+        var objectParams = { Bucket: bucketName, Key: keyName, Body: 'Hello World!' }; //<--Body means the content of the file.
+        // Create object upload promise
+        var uploadPromise = new AWS.S3({ apiVersion: '2006-03-01' }).putObject(objectParams).promise();
+        uploadPromise.then(
+          function (data) {
+            console.log("Successfully uploaded data to " + bucketName + "/" + keyName);
+          });
+      }).catch(
+        function (err) {
+          console.error(err, err.stack);
+        });
+  });
+
 };
-
-
